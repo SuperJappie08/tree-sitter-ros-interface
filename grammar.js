80e4a6e7fe5300d7c430794ed09e9cb15b0a1766
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2026-03-06 18:33:58 +0100

    Add action/service support
    Signed-off-by: SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>

--- grammar.js
+++ grammar.js
@@ -10,17 +10,72 @@
 export default grammar({
   name: "ros_interface",
 
-  extras: $ => [$.comment, /\s/],
+  extras: $ => [
+    /\s/,
+    $.comment
+  ],
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => seq($.field),
+    source_file: $ => choice(
+      field('message', repeat($._statement)),
+      $.service,
+      $.action,
+    ),
 
-    field: $ => seq($._type, " "),
+    service: $ => seq(
+      field('request', repeat($._statement)),
+      $._seperator,
+      field('response', repeat($._statement)),
+    ),
+
+    action: $ => seq(
+      field('goal', repeat($._statement)),
+      $._seperator,
+      field('result', repeat($._statement)),
+      $._seperator,
+      field('feedback', repeat($._statement)),
+    ),
+
+    _statement: $ => choice($.constant, $.field),
+
+    _seperator: $ => '---',
+
+
+    // TODO: DOC COMMENTS?
+    // TODO: @OPTIONAL
+    field: $ => seq(
+      field('type', $._type),
+      field('name', $.identifier),
+      optional(field('default', $.field_default)),
+    ),
+
+    constant: $ => seq(
+      field('type', $.primitive_type),
+      field('name', $.const_identifier),
+      '=',
+      field('value', $.primitive_value),
+    ),
+
+    identifier: $ => /[a-z](?:[a-z0-9_]?[a-z0-9]+)*/,
+
+    const_identifier: $ => /[A-Z](?:[A-Z0-9_]?[A-Z0-9]+)*/,
+
+    field_default: $ => $.value,
 
     _type: $ => seq($.base_type, optional($.array)),
 
-    base_type: $ => choice($.primitive_type),
+    base_type: $ => choice(
+      $.primitive_type,
+      $.custom_type,
+      $.external_custom_type,
+    ),
+
+    custom_type: $ => /[A-Z][A-Za-z0-9]*/,
+
+    package_specifier: $ => new RustRegex('([a-z](?:[a-z0-9_]?[a-z0-9]+)*)/'),
+
+    external_custom_type: $ => seq($.package_specifier, $.custom_type),
 
     primitive_type: $ => choice(
       'bool',
@@ -33,12 +88,40 @@ export default grammar({
       'time'
     ),
 
-    string_type: $ => seq(/w?string/, optional(seq('<=', $.length))),
+    string_type: $ => seq(/w?string/, optional(seq('<=', /\d+/))),
 
-    array: $ => seq('[', optional(seq(optional('<='), $.length)), ']'),
+    array: $ => seq('[', optional(token.immediate(seq(optional('<='), /\d+/))), ']'),
 
-    length: $ => /\d+/,
+    // _length: $ => token.immediate(/\d+/),
 
-    comment: $ => seq('#', /.*/)
+    comment: $ => seq(
+      '#',
+      /.*/,
+    ),
+
+    value: $ => choice(
+      $.primitive_value,
+      $.array_value,
+    ),
+
+    primitive_value: $ => choice($.int_value, $.string, $.float, $.bool),
+
+    string: $ => choice(/".*"/, /'.*'/),
+
+    // TODO: FIGURE OUT IF + is allowed????
+    int_value: $ => /-?\d+/,
+
+    float: $ => /[+-]?\d+\.\d+/,
+
+    bool: $ => choice(
+      'True',
+      'true',
+      '1',
+      'False',
+      'false',
+      '0',
+    ),
+
+    array_value: $ => seq('[', optional(seq(repeat(seq($.primitive_value, ',')), $.primitive_value)), ']')
   }
 });
